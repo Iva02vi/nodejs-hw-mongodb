a1c5aa489f884ba8d5fc86b81d7755288d1ccb03
Iva02vi <ivanova.valery.02@gmail.com>
2024-07-23 02:21:35 +0300

    all tasks

--- src/sevices/contacts.js
+++ src/sevices/contacts.js
@@ -1,14 +1,46 @@
+import { SORT_ORDER } from "../constants/index.js";
 import { Contact } from "../db/models/Contact.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 
-export const getAllContacts = async () => await Contact.find();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+
+  if (filter.type) {
+    contactsQuery.where('contactType').equals(filter.type);
+  }
+  if (filter.isFavourite) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    Contact.find().merge(contactsQuery).countDocuments(),
+    contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
+};
 
 export const getContactById = async (contactId) => await Contact.findById(contactId);
 
 export const createContact = async (data) => await Contact.create(data);
 
-export const updateContact = async (сontactId, payload, options = {}) => {
+export const updateContact = async (contactId, payload, options = {}) => {
   const rawResult = await Contact.findOneAndUpdate(
-    { _id: сontactId },
+    { _id: contactId },
     payload,
     {
       new: true,
@@ -29,3 +61,4 @@ export const deleteContact = async (contactId) => {
 
   return contact;
 };
+
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,27 @@
+const parseType = (type) => {
+  const isString = typeof type === 'string';
+  if (!isString) return;
+  const isType = (type) => ['work', 'home', 'personal'].includes(type);
+
+  if (isType(type)) return type;
+};
+
+const parseIsFavourite = (isFavourite) => {
+  const isString = typeof isFavourite === 'string';
+  if (!isString) return;
+  const isIsFavourite = (isFavourite) => ['true', 'false'].includes(isFavourite);
+
+  if (isIsFavourite(isFavourite)) return isFavourite;
+};
+
+export const parseFilterParams = (query) => {
+  const { type, isFavourite } = query;
+
+  const parsedType = parseType(type);
+  const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+  return {
+    type: parsedType,
+    isFavourite: parsedIsFavourite,
+  };
+};
